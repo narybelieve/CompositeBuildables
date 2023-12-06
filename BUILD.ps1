e756baa05547d42127d3e6f3cdca7e3713b60bd2
nary_believe <nary.believe@gmail.com>
2023-12-05 22:48:52 -0800

    Made build script loop

--- BUILD.ps1
+++ BUILD.ps1
@@ -1,3 +1,6 @@
-& 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe'
-Read-Host -Prompt "Press Enter to exit"
-# IF THIS COMPLAINS ABOUT NUGET, RUN "dotnet restore" not nuget
+while($true) {
+	cls
+	& 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe'
+	Read-Host -Prompt "Press Enter to restart"
+	# IF THIS COMPLAINS ABOUT NUGET, RUN "dotnet restore" not nuget
+}
